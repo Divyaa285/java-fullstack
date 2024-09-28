250d6b4886626807ec84eb735e81b87e6c91de2f
Your NameDivya <you@example.comsakundedivya2@gmail.com>
2024-09-29 00:21:55 +0530

    code added

---
+++ loop/nested for loop/p1.java
@@ -0,0 +1,16 @@
+class Demo{
+		public static void main (String[]args){
+	       char ch;
+		for(int i=1;i<=4;i++){
+		for(ch='A';ch<='D';ch++){
+			System.out.print(ch+" ");
+			}
+			System.out.println( );
+		}
+	}
+}
+  o/p = 
+A B C D
+A B C D
+A B C D
+A B C D
---
+++ loop/nested for loop/p6.java
@@ -0,0 +1,17 @@
+class Demo{
+		public static void main (String[]args){
+	      
+		for(int i=1;i<=3;i++){
+		for(int j=1;j<=3;j++){
+			System.out.print(" *_ ");
+			}
+			System.out.println( );
+			
+		}
+	}
+}
+
+o/p = 
+ *_  *_  *_
+ *_  *_  *_
+ *_  *_  *_
