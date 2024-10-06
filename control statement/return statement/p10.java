365ffe87f7daecdf73e41bcb011bc57ce00c462e
Your NameDivya <you@example.comsakundedivya2@gmail.com>
2024-10-06 22:10:34 +0530

    code added

---
+++ control statement/return statement/p10.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String []args){
+	char ch=fun();
+	System.out.print(ch);
+}
+	static char fun(){
+	System.out.println("in fun ");
+	return 65;
+}
+}
+
+O/P-- 30,-10
