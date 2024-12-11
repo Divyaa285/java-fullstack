ec0aae77a3e68172e851bc18be9ba6e85927cc49
Your NameDivya <you@example.comsakundedivya2@gmail.com>
2024-12-11 10:25:43 +0530

    Collection

---
+++ Oop (object oriented programming)/Collection/List Interface/ArrayList Method/p1.java
@@ -0,0 +1,15 @@
+
+
+import java.util.*;
+class Demo{
+	public static void main(String[]args){
+	ArrayList<String> ls = new ArrayList <String>();
+	ls.add("Shivam");
+	ls.add("Pranay");
+	ls.add("Nikhil");
+		System.out.println(ls);
+}
+	}
+
+
+			//[Shivam, Pranay, Nikhil]
